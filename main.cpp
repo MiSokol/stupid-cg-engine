b5b0b75aa4dfd075e2834ad61b9286201519d91a
misokol <so-mi-ig@yandex.ru>
2019-10-01 23:31:53 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "SDL.h"
+#include <random>
+#include <vector>
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+
+  std::cout << "Test working." << std::endl;
+
+  return 0;
+}
